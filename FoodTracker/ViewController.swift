e71c1b52432d44a02e87b9b573376a506692165d
Woozzang <woozzaang@gmail.com>
2020-11-24 20:25:04 +0900

    Add stack view to Meal Scene

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Woochan Park on 2020/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
