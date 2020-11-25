2aab394cf0d2c9b78b78ba872a2d79f95da91741
Woozzang <woozzaang@gmail.com>
2020-11-25 12:02:44 +0900

    Add custom view -RatingControl & Override initializer

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,26 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Woochan Park on 2020/11/25.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    //MARK: Initialization
+    
+    /*
+     For programatically initializing the view
+     */
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    /*
+     For loading the view from the storyboard
+     */
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
