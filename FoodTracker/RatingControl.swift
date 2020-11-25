83d14932401c3f9db69e990ef81a25c55f159387
Woozzang <woozzaang@gmail.com>
2020-11-25 17:48:35 +0900

    Add star-based rating function Align subviews of stackView to center

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -1,13 +1,38 @@
 //
 //  RatingControl.swift
-//  FoodTracker
+//  FoodTracker#imageLiteral(resourceName: "Screen Shot 2020-11-25 at 16.40.50.png")
 //
 //  Created by Woochan Park on 2020/11/25.
 //
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
+  
+  //MARK: Properties
+  private var ratingButtons = [UIButton]()
+  
+  var rating = 0 {
+    didSet {
+      updateButtonSelectionStates()
+    }
+  }
+  
+  // Need to reset the control’s buttons every time these attributes change
+  //  the didSet property observer is called immediately after the property’s value is set.
+  @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+    didSet {
+      setupButtons()
+    }
+  }
+  @IBInspectable var starCount: Int = 5 {
+    didSet {
+      setupButtons()
+    }
+  }
+  
+
+  
   //MARK: Initialization
   
   /*
@@ -27,25 +52,72 @@ class RatingControl: UIStackView {
   }
   
   private func setupButtons() {
+
+    // clear any existing buttons
+    for button in ratingButtons {
+      removeArrangedSubview(button)
+      button.removeFromSuperview()
+    }
+    ratingButtons.removeAll()
     
-    // Create the button
-    let button = UIButton()
-    button.backgroundColor = UIColor.systemRed
+    // Load Button Images
+    let bundle = Bundle(for: type(of:self))
+    let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+    let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
+    let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
     
-    // Add constraints
-    button.translatesAutoresizingMaskIntoConstraints = false
-    button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-    button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
     
-    // Setup the button action
-    button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
-    
-    // Add the button ot the stack
-    addArrangedSubview(button)
+    for _ in 0..<starCount {
+      // Create the button
+      let button = UIButton()
+      
+      // Set the button images
+      button.setImage(emptyStar, for: .normal)
+      button.setImage(filledStar, for: .selected)
+      button.setImage(highlightedStar, for: .highlighted)
+      button.setImage(highlightedStar, for: [.highlighted, .selected])
+      
+      
+      // Add constraints
+      button.translatesAutoresizingMaskIntoConstraints = false
+      button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+      button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+      
+      // Setup the button action
+      button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+      
+      // Add the button ot the stack
+      addArrangedSubview(button)
+      
+      // Add the new button to the rating button array
+      ratingButtons.append(button)
+    }
   }
   
+  private func updateButtonSelectionStates(){
+    for (index, button) in ratingButtons.enumerated(){
+      // If the index of a button is less than the rating, tha button should be selected.
+      button.isSelected = index < rating
+    }
+  }
+
   //MARK: Button Action
   @objc func ratingButtonTapped(button: UIButton) {
-    print("Button pressed 🇰🇷")
+    guard let index = ratingButtons.firstIndex(of: button) else {
+      fatalError("The button, \(button), is not in the ratignButotns array: \(ratingButtons)")
+    }
+    
+    // Calcluate the rating of the selected button
+    let selectedRating = index + 1
+    
+    if selectedRating == rating {
+      // If the selected star represents the current ratign, reset the rating to 0.
+      rating = 0
+    } else {
+      // Otherwise set the rating to the selected star
+      rating = selectedRating
+    }
+    
+    updateButtonSelectionStates()
   }
 }
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,102 +8,100 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    //MARK: Properties
-    @IBOutlet weak var mealNameLabel: UILabel!
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var photoImageView: UIImageView!
-    @IBOutlet weak var descriptionTextField: UITextField!
-    
-    //NOTE: 메서드를 통하지 않고 직접 IBOutlet 에 접근할 수 없다.
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        nameTextField.delegate = self
-        descriptionTextField.delegate = self
-    }
-    
-    //MARK: Actions
-    
-    // sender Type 만으로 어떤 타입의 View와 연결되었는지 짐작할 수 있다.
-    // Any 로 받으면 유추할 수 없다.
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
-    
-    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
-        
-        // Hide the keyboard.
-        if nameTextField.isFirstResponder {
-            nameTextField.resignFirstResponder()
-        }
-        
-        
-        let imagePickerController = UIImagePickerController()
-        
-        // Only allow photos to be picked, not taken.
-        imagePickerController.sourceType = .photoLibrary
-        
-        // Make sure ViewContorller is notified when the user picked an image.
-        imagePickerController.delegate = self
-        present(imagePickerController, animated: true, completion: nil)
-    }
+  
+  //MARK: Properties
+  @IBOutlet weak var mealNameLabel: UILabel!
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var photoImageView: UIImageView!
+  @IBOutlet weak var descriptionTextField: UITextField!
+  @IBOutlet weak var ratingControl: RatingControl!
+
+  //NOTE: 메서드를 통하지 않고 직접 IBOutlet 에 접근할 수 없다.
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      // Do any additional setup after loading the view.
+      nameTextField.delegate = self
+      descriptionTextField.delegate = self
+  }
+  
+  //MARK: Actions
+  
+  // sender Type 만으로 어떤 타입의 View와 연결되었는지 짐작할 수 있다.
+  // Any 로 받으면 유추할 수 없다.
+  
+  @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+      
+      // Hide the keyboard.
+      if nameTextField.isFirstResponder {
+          nameTextField.resignFirstResponder()
+      }
+      
+      
+      let imagePickerController = UIImagePickerController()
+      
+      // Only allow photos to be picked, not taken.
+      imagePickerController.sourceType = .photoLibrary
+      
+      // Make sure ViewContorller is notified when the user picked an image.
+      imagePickerController.delegate = self
+      present(imagePickerController, animated: true, completion: nil)
+  }
 }
 
 /*
- By adopting the UITextFieldDelegate protocol,
- you tell the compiler that the ViewController class can act as a valid text field delegate.
- 
- 즉, 적절한 delegate 프로토콜을 채택해야 유효한 delegate로 동작할 수 있다.
- 보통 새로운 protocol 을 채택할 때는 extension 을 이용해서 추가한다.
- */
+By adopting the UITextFieldDelegate protocol,
+you tell the compiler that the ViewController class can act as a valid text field delegate.
+
+즉, 적절한 delegate 프로토콜을 채택해야 유효한 delegate로 동작할 수 있다.
+보통 새로운 protocol 을 채택할 때는 extension 을 이용해서 추가한다.
+*/
 extension ViewController: UITextFieldDelegate {
-    
-    //MARK: UITextFieldDelegate
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
-        //Hide the keyboard.
-        textField.resignFirstResponder()
-        
-        guard textField === nameTextField else {
-            return true
-        }
-        
-        descriptionTextField.becomeFirstResponder()
-        return true
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        
-        mealNameLabel.text = textField.text
-    }
+  
+  //MARK: UITextFieldDelegate
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+      
+      //Hide the keyboard.
+      textField.resignFirstResponder()
+      
+      guard textField === nameTextField else {
+          return true
+      }
+      
+      descriptionTextField.becomeFirstResponder()
+      return true
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+      
+      mealNameLabel.text = textField.text
+  }
 }
 
 extension ViewController: UIImagePickerControllerDelegate {
-    
-    //MARK: UIImagePickerControllerDelegate
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        
-        // Dissmiss the picker if the user canceled.
-        dismiss(animated: true, completion: nil)
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
-            fatalError("Expected a dictionary containing an image, but was provided the following:\(info)")
-        }
-        
-        // Set photoView to display the selected image.
-        photoImageView.image = selectedImage
-        
-        // Dismiss the picker.
-        dismiss(animated: true, completion: nil)
-    }
+  
+  //MARK: UIImagePickerControllerDelegate
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+      
+      // Dissmiss the picker if the user canceled.
+      dismiss(animated: true, completion: nil)
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+      guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+          fatalError("Expected a dictionary containing an image, but was provided the following:\(info)")
+      }
+      
+      // Set photoView to display the selected image.
+      photoImageView.image = selectedImage
+      
+      // Dismiss the picker.
+      dismiss(animated: true, completion: nil)
+  }
 }
 
 extension ViewController: UINavigationControllerDelegate {
-    
+  
 }
