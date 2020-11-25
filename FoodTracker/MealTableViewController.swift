93b22aa2e8f076a9cf6c5246549cbd01455e30a7
Woozzang <woozzaang@gmail.com>
2020-11-26 00:45:45 +0900

    Implement sample initial meal data

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -10,32 +10,36 @@ import UIKit
 class MealTableViewController: UITableViewController {
 
   // MARK: Private Methods
+  var meals = [Meal]()
 
   override func viewDidLoad() {
       super.viewDidLoad()
+    
+    // Load the sample data.
+    loadSampleMeals()
   }
 
   // MARK: - Table view data source
 
   override func numberOfSections(in tableView: UITableView) -> Int {
-      // #warning Incomplete implementation, return the number of sections
-      return 0
+    // #warning Incomplete implementation, return the number of sections
+    return 1
   }
 
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      // #warning Incomplete implementation, return the number of rows
-      return 0
+    // #warning Incomplete implementation, return the number of rows
+    return meals.count
   }
 
-  /*
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-      // Configure the cell...
-
-      return cell
+    let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+    
+    //Configure the cell...
+    
+  
+    return cell
   }
-  */
+
 
   /*
   // Override to support conditional editing of the table view.
@@ -81,5 +85,26 @@ class MealTableViewController: UITableViewController {
       // Pass the selected object to the new view controller.
   }
   */
-
+  //MARK: Private Methods
+  
+  private func loadSampleMeals() {
+    
+    let photo1 = UIImage(named: "meal1")
+    let photo2 = UIImage(named: "meal2")
+    let photo3 = UIImage(named: "meal3")
+    
+    guard let meal1 = Meal(name: "Pepperoni Pizza", photo: photo1, rating: 4) else {
+      fatalError("Unable to instantiate meal1")
+    }
+    
+    guard let meal2 = Meal(name: "Tomato Pasta", photo: photo2, rating: 5) else {
+      fatalError("Unable to instantiate meal2")
+    }
+    
+    guard let meal3 = Meal(name: "Hamburger", photo: photo3, rating: 3) else {
+      fatalError("Unable to instantiate meal3")
+    }
+    
+    meals += [meal1, meal2, meal3]
+  }
 }
